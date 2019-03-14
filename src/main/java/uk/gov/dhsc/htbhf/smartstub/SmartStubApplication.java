25cc98980d0d193091314b7f0d7f6ddddef1275d
Mike Smith <mike.smith@sescon.co.uk>
2019-03-14 11:24:00 +0000

    HTBHF-632 Add request and session id to the log messages.

--- src/main/java/uk/gov/dhsc/htbhf/smartstub/SmartStubApplication.java
+++ src/main/java/uk/gov/dhsc/htbhf/smartstub/SmartStubApplication.java
@@ -2,11 +2,22 @@ package uk.gov.dhsc.htbhf.smartstub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.web.context.WebApplicationContext;
+import uk.gov.dhsc.htbhf.smartstub.requestcontext.RequestContext;
 
 @SpringBootApplication
 public class SmartStubApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SmartStubApplication.class, args);
+    }
+
+    @Bean
+    @Scope(value = WebApplicationContext.SCOPE_REQUEST, proxyMode = ScopedProxyMode.TARGET_CLASS)
+    public RequestContext requestContext() {
+        return new RequestContext();
     }
 }
